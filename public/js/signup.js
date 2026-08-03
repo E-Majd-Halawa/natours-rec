889b0556fa5a063b766b1af9cd811dfa28eda522
Mjad-Halawa <majodhalawa@gmail.com>
2026-08-03 18:37:33 +0300

    ...

--- public/js/signup.js
+++ public/js/signup.js
@@ -14,7 +14,7 @@ export const signup = async (name, email, password, passwordConfirm) => {
         passwordConfirm,
       },
     });
-
+    console.log('Signup Response:', res.data);
     if (res.data.status === 'success') {
       showAlert('success', 'Account created successfully!');
       window.setTimeout(() => {
@@ -22,6 +22,7 @@ export const signup = async (name, email, password, passwordConfirm) => {
       }, 1500);
     }
   } catch (err) {
+    console.error('Signup Error:', err.response ? err.response.data : err);
     showAlert('error', err.response.data.message);
   }
 };
