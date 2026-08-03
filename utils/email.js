344f2708bb9ddb7cb66b4b23b54e9b305b35037d
Mjad-Halawa <majodhalawa@gmail.com>
2026-08-03 18:12:14 +0300

    ...

--- utils/email.js
+++ utils/email.js
@@ -1,7 +1,8 @@
+const sgMail = require('@sendgrid/mail');
 const nodemailer = require('nodemailer');
 const pug = require('pug');
 const htmlToText = require('html-to-text');
-// new Email(user,url).sendWelcome()
+
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
@@ -9,47 +10,43 @@ module.exports = class Email {
     this.url = url;
     this.from = `Majd Halawa <${process.env.EMAIL_FROM}>`;
   }
-  newTransporter() {
-    if (process.env.NODE_ENV === 'production') {
-      return nodemailer.createTransport({
-        service: 'SendGrid',
-        auth: {
-          user: process.env.SENDGRID_USERNAME,
-          pass: process.env.SENDGRID_PASSWORD,
-        },
-      });
-    }
-    return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-      //Active in gmail "less secure app option"
-    });
-  }
+
   async send(template, subject) {
-    //1)Rander HTML based on apug template
     const html = pug.renderFile(
       `${__dirname}/../views/emails/${template}.pug`,
-      {
-        firstName: this.firstName,
-        url: this.url,
-        subject,
-      },
+      { firstName: this.firstName, url: this.url, subject },
     );
-    //2)Define email options
-    const mailOptions = {
-      from: this.from,
-      to: this.to,
-      subject,
-      html,
-      text: htmlToText.convert(html),
-      // html:
-    };
-    //3)Create a transport and send email
-    await this.newTransporter().sendMail(mailOptions);
+
+    const text = htmlToText.convert(html);
+
+    if (process.env.NODE_ENV === 'production') {
+      // Production: SendGrid via HTTPS API (works on Render)
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+      await sgMail.send({
+        to: this.to,
+        from: this.from,
+        subject,
+        html,
+        text,
+      });
+    } else {
+      // Development: local SMTP (mailtrap etc.)
+      const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+      await transporter.sendMail({
+        from: this.from,
+        to: this.to,
+        subject,
+        html,
+        text,
+      });
+    }
   }
 
   async sendWelcome() {
@@ -58,7 +55,7 @@ module.exports = class Email {
   async sendResetPassword() {
     await this.send(
       'passwordReset',
-      'Your prssword reset token(valid for only 10 minutes)',
+      'Your password reset token (valid for only 10 minutes)',
     );
   }
 };
