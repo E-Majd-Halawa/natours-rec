6c3e3f12f8d1e532d29482553d8feb5c15cabfb9
Mjad-Halawa <majodhalawa@gmail.com>
2026-08-04 13:22:30 +0300

    Improved Stripe Implementation

--- Controller/bookingController.js
+++ Controller/bookingController.js
@@ -47,16 +47,16 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 //   res.redirect(req.originalUrl.split('?')[0]);
 // });
 exports.createBookingCheckout = async (session) => {
-  const tour = client_reference_id;
+  const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
   let event;
   try {
-    event = stripe.webhook.constructEvent(
+    event = stripe.webhooks.constructEvent(
       req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET,
@@ -66,7 +66,7 @@ exports.webhookCheckout = (req, res, next) => {
   }
   if (event.type == 'checkout.session.completed')
     createBookingCheckout(event.data.object);
-  res.status(22).json({
+  res.status(200).json({
     received: true,
   });
 };
