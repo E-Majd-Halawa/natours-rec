421854b8a897091f60b952a0f77f9381d5b522a2
Mjad-Halawa <majodhalawa@gmail.com>
2026-08-05 15:04:55 +0300

    Implement manage users button

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const authController = require('../controller/authController');
-const userController = require('../controller/userController');
+const authController = require('../Controller/authController');
+const userController = require('../Controller/userController');
 
 const router = express.Router();
 
