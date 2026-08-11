307c63d0802f9d5d64a84d80d92df57c3a7ce075
Mjad-Halawa <majodhalawa@gmail.com>
2026-08-11 12:57:54 +0300

    Implement my become guide page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -90,6 +90,6 @@ router.get(
   viewController.getManageApplications,
 );
 // أضف السطر التالي مع باقي الـ Routes التي تعرض صفحات Pug
-router.get('/contact', viewsController.getContactForm);
+router.get('/contact', viewController.getContactForm);
 router.get('/download-apps', viewController.getDownloadApps);
 module.exports = router;
